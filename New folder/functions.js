90e4d0bd83da3a9741d3abf99294736e0cfbdac1
Omran-S <oversm9@gmail.com>
2023-07-03 11:24:10 +0300

    new project

---
+++ New folder/functions.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+    var pageIndex = 0;
+    var layoutNum = 0;
+
+    if($(document).height() / $(document).width() >= 2)
+        $("html").css('font-size','2vh');
+
+    $("#NextContent").on("click", function () {
+        if (pageIndex != 28)
+            pageIndex++;
+        $.getJSON("data.json", function (data) {
+            if (data[pageIndex].pageNumber == 5 || data[pageIndex].pageNumber == 24) {
+                $("#letter").css("margin-bottom", "20px");
+            } else {
+                $("#letter").css("margin-bottom", "0px");
+                $("#letter").css("margin-top", "0px");
+            }
+            $("main").css("box-shadow", data[pageIndex].borderColor);
+            $("#headerPageNumberBackground").css("background-color", data[pageIndex].pageNumberColor);
+            $("#headerPageNumber").text(data[pageIndex].pageNumber);
+            $("#letter").text(data[pageIndex].pageLetter);
+
+            $("#1stShape").text(data[pageIndex].firstShape);
+            $("#firstPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-1.jpg");
+            $("#1stPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].first);
+            $("#2ndPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].second);
+            $("#3rdPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].third);
+            $("#1stExampleEn").text(data[pageIndex].firstExampleEn);
+
+            $("#2ndShape").text(data[pageIndex].secondShape);
+            $("#secondPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-2.jpg");
+            $("#1stPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].first);
+            $("#2ndPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].second);
+            $("#3rdPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].third);
+            $("#2ndExampleEn").text(data[pageIndex].secondExampleEn);
+
+            $("#3rdShape").text(data[pageIndex].thirdShape);
+            $("#thirdPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-3.jpg");
+            $("#1stPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].first);
+            $("#2ndPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].second);
+            $("#3rdPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].third);
+            $("#3rdExampleEn").text(data[pageIndex].thirdExampleEn);
+
+            $("#footerPageNumber").text(data[pageIndex].pageNumber);
+        });
+    });
+    $("#PreviousContent").on("click", function () {
+        console.log(pageIndex);
+        if (pageIndex > 0) {
+            pageIndex--;
+            $.getJSON("data.json", function (data) {
+                if (data[pageIndex].pageNumber == 1) {
+                    $("#letter").css("margin-top", "15px");
+                }
+
+                $("main").css("box-shadow", data[pageIndex].borderColor);
+                $("#headerPageNumberBackground").css("background-color", data[pageIndex].pageNumberColor);
+                $("#headerPageNumber").text(data[pageIndex].pageNumber);
+                $("#letter").text(data[pageIndex].pageLetter);
+
+                $("#1stShape").text(data[pageIndex].firstShape);
+                $("#firstPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-1.jpg");
+                $("#1stPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].first);
+                $("#2ndPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].second);
+                $("#3rdPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].third);
+                $("#1stExampleEn").text(data[pageIndex].firstExampleEn);
+
+                $("#2ndShape").text(data[pageIndex].secondShape);
+                $("#secondPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-2.jpg");
+                $("#1stPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].first);
+                $("#2ndPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].second);
+                $("#3rdPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].third);
+                $("#2ndExampleEn").text(data[pageIndex].secondExampleEn);
+
+                $("#3rdShape").text(data[pageIndex].thirdShape);
+                $("#thirdPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-3.jpg");
+                $("#1stPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].first);
+                $("#2ndPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].second);
+                $("#3rdPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].third);
+                $("#3rdExampleEn").text(data[pageIndex].thirdExampleEn);
+
+                $("#footerPageNumber").text(data[pageIndex].pageNumber);
+            });
+        }
+    });
+    $("#NextContent").on("click", function () {
+        if (pageIndex >= 0 && layoutNum != 1) {
+            $(".onlyShape").remove();
+            $(".allShapes").append(secondLayout);
+            layoutNum = 1;
+        }
+    });
+    $("#PreviousContent").on("click", function () {
+        if (pageIndex == 0 && layoutNum != 0) {
+            $(".shapes").remove();
+            $(".allShapes").append(firstLayout);
+            layoutNum = 0;
+        }
+    });   
+});
---
+++ New folder/layouts.js
@@ -0,0 +1,93 @@
+const firstLayout =
+    `<div class="onlyShape">
+<p class="arExplain">
+    حرف <span class="redColor">الألف</span> لا یقبل الحركة ویأتي دوما
+    <span class="redColor">ساكن</span> وما قبلھ <span class="redColor">مفتوح</span>.
+</p>
+<div class="onlyExample">
+    <h3>ـَـ<span class="redColor">ا</span></h3>
+</div>
+<p class="enExplain">
+
+    <span class="redColor" style="font-style: italic;">Al-Alif</span> is always <span class="redColor" style="font-style: italic;">sakin</span> and it is
+    always preceded by <span class="redColor" style="font-style: italic;">Al-Fathah</span>.
+
+</p>
+</div>`;
+
+const secondLayout =
+    `<div class="shapes">
+        <div class="letterPicture">
+            <div class="picture">
+                <img id="firstPic" src="images/pic-2-1.jpg" alt="">
+            </div>
+            <div class="englishWord">
+                <h3 id="1stExampleEn">im here</h3>
+            </div>
+        </div>
+        <div class="example">
+            <div class="overlay"></div>
+            <div class="arabicWord">
+                <h3>
+                    <span id="1stPartOfFirstWord"></span><span id="2ndPartOfFirstWord"
+                        class="redColor"></span><span id="3rdPartOfFirstWord"></span>
+                </h3>
+                <span class="line"></span>
+            </div>
+            <div class="letterForm">
+                <div class="overlay"></div>
+                <h3 id="1stShape" class="redColor">بـا</h3>
+                <span class="line"></span>
+            </div>
+        </div>
+    </div>
+    <div class="shapes">
+        <div class="letterPicture">
+            <div class="picture">
+                <img id="secondPic" src="ثعبان.jpg" alt="">
+            </div>
+            <div class="englishWord">
+                <h3 id="2ndExampleEn">im here</h3>
+            </div>
+        </div>
+        <div class="example">
+            <div class="overlay"></div>
+            <div class="arabicWord">
+                <h3>
+                    <span id="1stPartOfSecondWord"></span><span id="2ndPartOfSecondWord"
+                        class="greenColor"></span><span id="3rdPartOfSecondWord"></span>
+                </h3>
+                <span class="line"></span>
+            </div>
+            <div class="letterForm">
+                <div class="overlay"></div>
+                <h3 id="2ndShape" class="greenColor">بـا</h3>
+                <span class="line"></span>
+            </div>
+        </div>
+    </div>
+    <div class="shapes">
+        <div class="letterPicture">
+            <div class="picture">
+                <img id="thirdPic" src="ثعبان.jpg" alt="">
+            </div>
+            <div class="englishWord">
+                <h3 id="3rdExampleEn">im here</h3>
+            </div>
+        </div>
+        <div class="example">
+            <div class="overlay"></div>
+            <div class="arabicWord">
+                <h3>
+                    <span id="1stPartOfThirdWord"></span><span id="2ndPartOfThirdWord"
+                        class="blueColor"></span><span id="3rdPartOfThirdWord"></span>
+                </h3>
+                <span class="line"></span>
+            </div>
+            <div class="letterForm">
+                <div class="overlay"></div>
+                <h3 id="3rdShape" class="blueColor">بـا</h3>
+                <span class="line"></span>
+            </div>
+        </div>
+    </div>`;
