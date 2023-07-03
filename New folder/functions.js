b44690ad6948685096d58764542798fe3bc81047
Omran-S <oversm9@gmail.com>
2023-07-03 11:51:49 +0300

    Update functions.js

--- New folder/functions.js
+++ New folder/functions.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
     var pageIndex = 0;
     var layoutNum = 0;
-    alert($(document).height());
-    alert($(document).width());
 
-    if($(document).height() / $(document).width() >= 1)
+    if($(document).height() / $(document).width() >= 1.75)
         $("html").css('font-size','2vh');
 
     $("#NextContent").on("click", function () {
