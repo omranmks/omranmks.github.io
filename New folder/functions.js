cd011b2bf79baa64705233f497b5e42fd5e3b7c5
Omran-S <oversm9@gmail.com>
2023-07-08 14:34:42 +0300

    updated

--- New folder/functions.js
+++ New folder/functions.js
@@ -1,100 +1,154 @@
+
 $(document).ready(function () {
+    if ($(document).height() / $(document).width() >= 1.75)
+        $("html").css('font-size', '2vh');
+
     var pageIndex = 0;
-    var layoutNum = 0;
+    var layoutNum = 1;
+    var letterIndex = 28;
+    var check = false;
+    $("#PreviousContent").hide();
 
-    if($(document).height() / $(document).width() >= 1.75)
-        $("html").css('font-size','2vh');
+    
 
-    $("#NextContent").on("click", function () {
-        if (pageIndex != 28)
-            pageIndex++;
-        $.getJSON("data.json", function (data) {
-            if (data[pageIndex].pageNumber == 5 || data[pageIndex].pageNumber == 24) {
-                $("#letter").css("margin-bottom", "20px");
-            } else {
-                $("#letter").css("margin-bottom", "0px");
-                $("#letter").css("margin-top", "0px");
+    $("input").change(function () {
+        chageInput();
+    });
+    function chageInput(){
+        check = !check;
+        if(check)
+        {
+            $(".mainLayout").append(indexPage);
+            $(".indexPage").animate({opacity: 1},400);
+        }
+        else{
+            $(".indexPage").animate({opacity: 0},400,'swing',function() {
+                $(".indexPage").remove();
+            }); 
+        }
+    }
+    $(document).on('change', function () { 
+        $(".indexPage").on("click", ".overlay", function(e) {
+            pageIndex = parseInt($(e.target).attr('value'));
+            $('input').prop('checked', false);
+            $('input').trigger('change');
+            ChangeLayout();
+        });  
+     });
+
+    function ChangeLayout() {
+        if (pageIndex == 0) {
+            $("#PreviousContent").hide();
+        }
+        else if (pageIndex == 34) {
+            $("#NextContent").hide();
+        }
+        else {
+            $("#PreviousContent").show();
+            $("#NextContent").show();
+        }
+
+        if (pageIndex == 0) {
+            if (layoutNum != 1) {
+                $(".table").remove();
+                $(".numBox").remove();
+                $(".allShapes").remove();
+                $(".header").after(firstLayout);
+                layoutNum = 1;
             }
-            $("main").css("box-shadow", data[pageIndex].borderColor);
-            $("#headerPageNumberBackground").css("background-color", data[pageIndex].pageNumberColor);
-            $("#headerPageNumber").text(data[pageIndex].pageNumber);
-            $("#letter").text(data[pageIndex].pageLetter);
-
-            $("#1stShape").text(data[pageIndex].firstShape);
-            $("#firstPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-1.jpg");
-            $("#1stPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].first);
-            $("#2ndPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].second);
-            $("#3rdPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].third);
-            $("#1stExampleEn").text(data[pageIndex].firstExampleEn);
-
-            $("#2ndShape").text(data[pageIndex].secondShape);
-            $("#secondPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-2.jpg");
-            $("#1stPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].first);
-            $("#2ndPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].second);
-            $("#3rdPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].third);
-            $("#2ndExampleEn").text(data[pageIndex].secondExampleEn);
-
-            $("#3rdShape").text(data[pageIndex].thirdShape);
-            $("#thirdPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-3.jpg");
-            $("#1stPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].first);
-            $("#2ndPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].second);
-            $("#3rdPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].third);
-            $("#3rdExampleEn").text(data[pageIndex].thirdExampleEn);
-
-            $("#footerPageNumber").text(data[pageIndex].pageNumber);
-        });
-    });
-    $("#PreviousContent").on("click", function () {
-        console.log(pageIndex);
-        if (pageIndex > 0) {
-            pageIndex--;
-            $.getJSON("data.json", function (data) {
-                if (data[pageIndex].pageNumber == 1) {
-                    $("#letter").css("margin-top", "15px");
+        }
+        else if (pageIndex <= 28) {
+            if (layoutNum != 2) {
+                $(".table").remove();
+                $(".numBox").remove();
+                $(".allShapes").remove();
+                $(".header").after(secondLayout);
+                layoutNum = 2;
+            }
+        } else if (pageIndex <= 33) {
+            if (layoutNum != 3) {
+                $(".table").remove();
+                $(".numBox").remove();
+                $(".allShapes").remove();
+                $(".header").after(thirdLayout);
+                layoutNum = 3;
+            }
+        } else if (pageIndex <= 34) {
+            if (layoutNum != 4) {
+                $(".table").remove();
+                $(".numBox").remove();
+                $(".allShapes").remove();
+                $(".header").after(forthLayout);
+                layoutNum = 4;
+            }
+        }
+        ChangeData();
+    }
+    function ChangeData() {
+        $.getJSON("data.json",
+            function (data) {
+                if(pageIndex == 4 || pageIndex == 23){
+                    $("#letter").css('margin-bottom','1rem');
                 }
+                else{
+                    $("#letter").css('margin-bottom','0.35rem');
+                }
+                if (pageIndex <= 28) {
+                    $("main").css('box-shadow', data[pageIndex].borderColor);
+                    $("#headerPageNumberBackground").css('background-color', data[pageIndex].pageNumberColor);
+                    $("#headerPageNumber").text(data[pageIndex].pageNumber);
+                    $("#letter").text(data[pageIndex].pageLetter);
+                    for (var i = 1; i <= 3; i++) {
+                        $("#pic-" + i).attr('src', 'images/pic-' + (pageIndex + 1) + '-' + i + '.jpg');
+                        $("#enWord-" + i).text(data[pageIndex]['en-' + i]);
+                        $("#shape-" + i).text(data[pageIndex]['shape-' + i]);
+                        for (var j = 1; j <= 3; j++) {
+                            $("#" + i + "-p" + j).text(data[pageIndex]['ar-' + i][0]['p-' + j]);
+                        }
+                    }
+                    $("#footerPageNumber").text('- ' + data[pageIndex].pageNumber + ' -');
+                }
+                else if (pageIndex <= 33) {
+                    $("main").css('box-shadow', "#afafaf 0px 0px 0px 1rem inset");
+                    for (var i = 0; i < 5; i++) {
+                        $("#" + (i + 1) + "-NumColor").css('background-color', data[pageIndex - letterIndex + i].pageNumberColor);
+                        $("#" + (i + 1) + "-NumColor").text(data[pageIndex - letterIndex + i].pageNumber);
+                        for (var j = 1; j <= 3; j++) {
+                            $("#" + (i + 1) + "-" + j).text(data[pageIndex - letterIndex + i]['shape-' + j]);
+                        }
+                    }
+                    $("#footerPageNumber").text('- ' + (pageIndex + 1) + ' -');
 
-                $("main").css("box-shadow", data[pageIndex].borderColor);
-                $("#headerPageNumberBackground").css("background-color", data[pageIndex].pageNumberColor);
-                $("#headerPageNumber").text(data[pageIndex].pageNumber);
-                $("#letter").text(data[pageIndex].pageLetter);
+                }
+                else if (pageIndex == 34) {
+                    $("main").css('box-shadow', "#afafaf 0px 0px 0px 1rem inset");
+                    for (var i = 0; i < 3; i++) {
+                        $("#" + (i + 1) + "-NumColor").css('background-color', data[pageIndex - letterIndex + i].pageNumberColor);
+                        $("#" + (i + 1) + "-NumColor").text(data[pageIndex - letterIndex + i].pageNumber);
+                        for (var j = 1; j <= 3; j++) {
+                            $("#" + (i + 1) + "-" + j).text(data[pageIndex - letterIndex + i]['shape-' + j]);
+                        }
+                    }
+                    $("#footerPageNumber").text('- ' + (pageIndex + 1) + ' -');
 
-                $("#1stShape").text(data[pageIndex].firstShape);
-                $("#firstPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-1.jpg");
-                $("#1stPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].first);
-                $("#2ndPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].second);
-                $("#3rdPartOfFirstWord").text(data[pageIndex].firstExampleAr[0].third);
-                $("#1stExampleEn").text(data[pageIndex].firstExampleEn);
-
-                $("#2ndShape").text(data[pageIndex].secondShape);
-                $("#secondPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-2.jpg");
-                $("#1stPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].first);
-                $("#2ndPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].second);
-                $("#3rdPartOfSecondWord").text(data[pageIndex].secondExampleAr[0].third);
-                $("#2ndExampleEn").text(data[pageIndex].secondExampleEn);
-
-                $("#3rdShape").text(data[pageIndex].thirdShape);
-                $("#thirdPic").attr("src", "images/pic-" + data[pageIndex].pageNumber + "-3.jpg");
-                $("#1stPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].first);
-                $("#2ndPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].second);
-                $("#3rdPartOfThirdWord").text(data[pageIndex].thirdExampleAr[0].third);
-                $("#3rdExampleEn").text(data[pageIndex].thirdExampleEn);
-
-                $("#footerPageNumber").text(data[pageIndex].pageNumber);
-            });
-        }
-    });
+                }
+            }
+        );
+    }
     $("#NextContent").on("click", function () {
-        if (pageIndex >= 0 && layoutNum != 1) {
-            $(".onlyShape").remove();
-            $(".allShapes").append(secondLayout);
-            layoutNum = 1;
+        if (pageIndex != 34) {
+            pageIndex++;
+            if (pageIndex > 29)
+                letterIndex -= 4;
         }
+        ChangeLayout();
     });
     $("#PreviousContent").on("click", function () {
-        if (pageIndex == 0 && layoutNum != 0) {
-            $(".shapes").remove();
-            $(".allShapes").append(firstLayout);
-            layoutNum = 0;
+        if (pageIndex != 0) {
+            pageIndex--;
+            if (pageIndex >= 29)
+                letterIndex += 4;
         }
-    });   
+        ChangeLayout();
+    });
 });
--- New folder/layouts.js
+++ New folder/layouts.js
@@ -1,42 +1,64 @@
 const firstLayout =
-    `<div class="onlyShape">
-<p class="arExplain">
-    حرف <span class="redColor">الألف</span> لا یقبل الحركة ویأتي دوما
-    <span class="redColor">ساكن</span> وما قبلھ <span class="redColor">مفتوح</span>.
-</p>
-<div class="onlyExample">
-    <h3>ـَـ<span class="redColor">ا</span></h3>
+    `<div class="numBox">
+    <div class="overlay"></div>
+    <div id="headerPageNumberBackground" class="pageNumber" style="background-color: #FF0000;">
+        <h2 id="headerPageNumber">1</h2>
+    </div>
+    <div class="pageLetter">
+        <h2 id="letter" style="margin-bottom: 0px; margin-top: 15px;">أَلِـف</h2>
+    </div>
 </div>
-<p class="enExplain">
+<div class="allShapes">
+    <div class="onlyShape">
+        <p class="arExplain">
+            حرف <span class="redColor">الألف</span> لا یقبل الحركة ویأتي دوما
+            <span class="redColor">ساكن</span> وما قبلھ <span class="redColor">مفتوح</span>.
+        </p>
+        <div class="onlyExample">
+            <h3>ـَـ<span class="redColor">ا</span></h3>
+        </div>
+        <p class="enExplain">
 
-    <span class="redColor" style="font-style: italic;">Al-Alif</span> is always <span class="redColor" style="font-style: italic;">sakin</span> and it is
-    always preceded by <span class="redColor" style="font-style: italic;">Al-Fathah</span>.
+            <span class="redColor" style="font-style: italic;">Al-Alif</span> is always <span
+                class="redColor" style="font-style: italic;">sakin</span> and it is
+            always preceded by <span class="redColor" style="font-style: italic;">Al-Fathah</span>.
 
-</p>
+        </p>
+    </div>
 </div>`;
 
 const secondLayout =
-    `<div class="shapes">
+    `<div class="numBox">
+    <div class="overlay"></div>
+    <div id="headerPageNumberBackground" class="pageNumber" style="background-color: #FF0000;">
+        <h2 id="headerPageNumber">1</h2>
+    </div>
+    <div class="pageLetter">
+        <h2 id="letter" style="margin-bottom: 0px; margin-top: 15px;">أَلِـف</h2>
+    </div>
+</div>
+<div class="allShapes">
+    <div class="shapes">
         <div class="letterPicture">
             <div class="picture">
-                <img id="firstPic" src="images/pic-2-1.jpg" alt="">
+                <img id="pic-1" src="images/pic-2-1.jpg" alt="">
             </div>
             <div class="englishWord">
-                <h3 id="1stExampleEn">im here</h3>
+                <h3 id="enWord-1">im here</h3>
             </div>
         </div>
         <div class="example">
             <div class="overlay"></div>
             <div class="arabicWord">
                 <h3>
-                    <span id="1stPartOfFirstWord"></span><span id="2ndPartOfFirstWord"
-                        class="redColor"></span><span id="3rdPartOfFirstWord"></span>
+                    <span id="1-p1"></span><span id="1-p2"
+                        class="redColor"></span><span id="1-p3"></span>
                 </h3>
                 <span class="line"></span>
             </div>
             <div class="letterForm">
                 <div class="overlay"></div>
-                <h3 id="1stShape" class="redColor">بـا</h3>
+                <h3 id="shape-1" class="redColor">بـا</h3>
                 <span class="line"></span>
             </div>
         </div>
@@ -44,24 +66,24 @@ const secondLayout =
     <div class="shapes">
         <div class="letterPicture">
             <div class="picture">
-                <img id="secondPic" src="ثعبان.jpg" alt="">
+                <img id="pic-2" src="ثعبان.jpg" alt="">
             </div>
             <div class="englishWord">
-                <h3 id="2ndExampleEn">im here</h3>
+                <h3 id="enWord-2">im here</h3>
             </div>
         </div>
         <div class="example">
             <div class="overlay"></div>
             <div class="arabicWord">
                 <h3>
-                    <span id="1stPartOfSecondWord"></span><span id="2ndPartOfSecondWord"
-                        class="greenColor"></span><span id="3rdPartOfSecondWord"></span>
+                    <span id="2-p1"></span><span id="2-p2"
+                        class="greenColor"></span><span id="2-p3"></span>
                 </h3>
                 <span class="line"></span>
             </div>
             <div class="letterForm">
                 <div class="overlay"></div>
-                <h3 id="2ndShape" class="greenColor">بـا</h3>
+                <h3 id="shape-2" class="greenColor">بـا</h3>
                 <span class="line"></span>
             </div>
         </div>
@@ -69,25 +91,297 @@ const secondLayout =
     <div class="shapes">
         <div class="letterPicture">
             <div class="picture">
-                <img id="thirdPic" src="ثعبان.jpg" alt="">
+                <img id="pic-3" src="ثعبان.jpg" alt="">
             </div>
             <div class="englishWord">
-                <h3 id="3rdExampleEn">im here</h3>
+                <h3 id="enWord-3">im here</h3>
             </div>
         </div>
         <div class="example">
             <div class="overlay"></div>
             <div class="arabicWord">
                 <h3>
-                    <span id="1stPartOfThirdWord"></span><span id="2ndPartOfThirdWord"
-                        class="blueColor"></span><span id="3rdPartOfThirdWord"></span>
+                    <span id="3-p1"></span><span id="3-p2"
+                        class="blueColor"></span><span id="3-p3"></span>
                 </h3>
                 <span class="line"></span>
             </div>
             <div class="letterForm">
                 <div class="overlay"></div>
-                <h3 id="3rdShape" class="blueColor">بـا</h3>
+                <h3 id="shape-3" class="blueColor">بـا</h3>
                 <span class="line"></span>
             </div>
         </div>
-    </div>`;
+    </div>
+</div>`;
+
+const thirdLayout =
+    `<div class="table">
+    <div class="eachShape">
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="1-3" class="blueColor">بُـو</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="1-2" class="greenColor">بِـي</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="1-NumColor" class="circleNumber" style="background-color: #FF0000;">2</div>
+            <div id="1-1" class="redColor">بَـا</div>
+        </div>
+    </div>
+    <div class="eachShape">
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="2-3" class="blueColor">تُـو</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="2-2" class="greenColor">تِـي</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="2-NumColor" class="circleNumber" style="background-color: #FF0000;">3</div>
+            <div id="2-1" class="redColor">تَـا</div>
+        </div>
+    </div>
+    <div class="eachShape">
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="3-3" class="blueColor">ثُـو</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="3-2" class="greenColor">ثِـي</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="3-NumColor" class="circleNumber" style="background-color: #FF0000;">4</div>
+            <div id="3-1" class="redColor">ثَـا</div>
+        </div>
+    </div>
+    <div class="eachShape">
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="4-3" class="blueColor">جُـو</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="4-2" class="greenColor">جِـي</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="4-NumColor" class="circleNumber" style="background-color: #FF0000;">5</div>
+            <div id="4-1" class="redColor">جَـا</div>
+        </div>
+    </div>
+    <div class="eachShape">
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="5-3" class="blueColor">حُـو</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="5-2" class="greenColor">حِـي</div>
+        </div>
+        <div class="eachLetter">
+            <div class="overlay"></div>
+            <div id="5-NumColor" class="circleNumber" style="background-color: #FF0000;">6</div>
+            <div id="5-1" class="redColor">حَـا</div>
+        </div>
+    </div>
+</div>`;
+
+const forthLayout = 
+`<div class="table">
+<div class="eachShape">
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="1-3" class="blueColor">بُـو</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="1-2" class="greenColor">بِـي</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="1-NumColor" class="circleNumber" style="background-color: #FF0000;">2</div>
+        <div id="1-1" class="redColor">بَـا</div>
+    </div>
+</div>
+<div class="eachShape">
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="2-3" class="blueColor">تُـو</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="2-2" class="greenColor">تِـي</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="2-NumColor" class="circleNumber" style="background-color: #FF0000;">3</div>
+        <div id="2-1" class="redColor">تَـا</div>
+    </div>
+</div>
+<div class="eachShape">
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="3-3" class="blueColor">ثُـو</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="3-2" class="greenColor">ثِـي</div>
+    </div>
+    <div class="eachLetter">
+        <div class="overlay"></div>
+        <div id="3-NumColor" class="circleNumber" style="background-color: #FF0000;">4</div>
+        <div id="3-1" class="redColor">ثَـا</div>
+    </div>
+</div>                    
+</div>`;
+const indexPage =
+`<div class="indexPage">
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #bdd7ee;">
+        <div class="overlay" value='2'></div>
+        <h3>تَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #bdd7ee;">
+        <div class="overlay" value='1'></div>
+        <h3>بَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #ff0000;">
+        <div class="overlay" value='0'></div>
+        <h3>ألف</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #ffe699;">
+        <div class="overlay" value='5'></div>
+        <h3>حَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #ffe699;">
+        <div class="overlay" value='4'></div>
+        <h3 style="margin-bottom: 1.2rem;">جـِيـم</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #bdd7ee;">
+        <div class="overlay" value='3'></div>
+        <h3>ثَـا</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #f8ccad;">
+        <div class="overlay" value='8'></div>
+        <h3>ذَال</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #f8ccad;">
+        <div class="overlay" value='7'></div>
+        <h3>دَال</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #ffe699;">
+        <div class="overlay" value='6'></div>
+        <h3>خَـا</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #d6dce5;">
+        <div class="overlay" value='11'></div>
+        <h3>سِـيـن</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #bf910a;">
+        <div class="overlay" value='10'></div>
+        <h3>زَاي</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #bf910a;">
+        <div class="overlay" value='9'></div>
+        <h3>رَا</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #2f5598;">
+        <div class="overlay" value='14'></div>
+        <h3>ضَـاد</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #2f5598;">
+        <div class="overlay" value='13'></div>
+        <h3>صَـاد</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #d6dce5;">
+        <div class="overlay" value='12'></div>
+        <h3>شِـيـن</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #afabab;">
+        <div class="overlay" value='17'></div>
+        <h3>عَـيْـن</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #203863;">
+        <div class="overlay" value='16'></div>
+        <h3>ظَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #203863;">
+        <div class="overlay" value='15'></div>
+        <h3>طَـا</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #f7ad7c;">
+        <div class="overlay" value='20'></div>
+        <h3>قَـاف</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #f7ad7c;">
+        <div class="overlay" value='19'></div>
+        <h3>فَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #afabab;">
+        <div class="overlay" value='18'></div>
+        <h3>غَـيْـن</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #29a951;">
+        <div class="overlay" value='23'></div>
+        <h3 style="margin-bottom: 1.2rem;">مِـيـم</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #27ade4;">
+        <div class="overlay" value='22'></div>
+        <h3>لَام</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #673a8e;">
+        <div class="overlay" value='21'></div>
+        <h3>كَـاف</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #b6db9a;">
+        <div class="overlay" value='26'></div>
+        <h3>وَاو</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #fab613;">
+        <div class="overlay" value='25'></div>
+        <h3>هَـا</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #385725;">
+        <div class="overlay" value='24'></div>
+        <h3>نُـون</h3>
+    </div>
+</div>
+<div class="rowLetters">
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #afafaf;">
+        <div class="overlay" value='29'></div>
+        <h3>أمثلة</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #cbc9e6;">
+        <div class="overlay" value='28'></div>
+        <h3>هَـمْـزَة</h3>
+    </div>
+    <div class="letterIndex" style="box-shadow: inset 0 0 0 0.3rem #ffec00;">
+        <div class="overlay" value='27'></div>
+        <h3>يَـا</h3>
+    </div>
+</div>
+</div>`;
